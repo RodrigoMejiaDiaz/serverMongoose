6a0e41471ae4ddb93d9f0d7c17f297fb634ce305
RodrigoErnestoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2023-03-01 13:01:06 -0500

    api productois

---
+++ models/Producto.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ProductoSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  marca: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Producto = mongoose.model("producto", ProductoSchema);
--- routes/api/categorias.js
+++ routes/api/categorias.js
@@ -4,14 +4,14 @@ const router = express.Router();
 // Load Book model
 const Categoria = require("../../models/Categoria");
 
-router.get("/test", (req, res) => res.send("book route testing!"));
+router.get("/test", (req, res) => res.send("categoria route testing!"));
 
 // @route GET api/categorias
 // @description Get all categorias
 // @access Public
 router.get("/categorias", (req, res) => {
   Categoria.find()
-    .then(categorias => res.json(categorias))
+    .then((categorias) => res.json(categorias))
     .catch((err) =>
       res.status(404).json({ nohaycategoria: "No Categoria encontrada" })
     );
@@ -28,4 +28,28 @@ router.get("/categorias/:id", (req, res) => {
     );
 });
 
+// Load Producto model
+const Producto = require("../../models/Producto");
+
+// @route GET api/productos
+// @description Get all productos
+// @access Public
+router.get("/productos", (req, res) => {
+  Producto.find()
+    .then((productos) => res.json(productos))
+    .catch((err) =>
+      res.status(404).json({ nohayproductos: "No Productos encontrados" })
+    );
+});
+
+// @route GET api/productos/:id
+// @description Get single producto by id
+// @access Public
+router.get("/productos/:id", (req, res) => {
+  Producto.findById(req.params.id)
+    .then((producto) => res.json(producto))
+    .catch((err) =>
+      res.status(404).json({ nohayproductos: "No Productos encontrados" })
+    );
+});
 module.exports = router;
